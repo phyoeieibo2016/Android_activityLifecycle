9a8c00d562527909c320eb518afa753f3be41b77
phyoeiei.bo <phyoeieibo.peeb@gmail.com>
2020-03-24 15:08:56 +0630

    show log of activity lifecycle.

---
+++ app/src/main/java/mm/gov/activitylifecycle/MainActivity.java
@@ -0,0 +1,51 @@
+package mm.gov.activitylifecycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String HOME_ACTIVITY_TAG = MainActivity.class.getSimpleName();
+
+    private void showLog(String text){
+        Log.d(HOME_ACTIVITY_TAG, text);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        showLog("Activity is creating....");
+    }
+
+    protected void onRestart(){
+        super.onRestart();
+        showLog("Activity restarted");
+    }
+
+    protected void onStart(){
+        super.onStart();
+        showLog("Activity is Starting...");
+    }
+
+    protected void onResume(){
+        super.onResume();
+        showLog("Activity Resumed");
+    }
+
+    protected void onPause(){
+        super.onPause();
+        showLog("Activity Paused");
+    }
+
+    protected void onStop(){
+        super.onStop();
+        showLog("Activity Stopped");
+    }
+
+    protected void onDestory(){
+        super.onDestroy();
+        showLog("Activity has been destoryed !");
+    }
+}
